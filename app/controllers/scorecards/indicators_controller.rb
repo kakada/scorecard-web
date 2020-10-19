3d6c8711516c5d80bbbe9acfe3aaff1d750bc3f1
Sokly <hengsokly23@gmail.com>
2020-10-19 17:24:06 +0700

    refactor

--- app/controllers/scorecards/indicators_controller.rb
+++ app/controllers/scorecards/indicators_controller.rb
@@ -4,16 +4,7 @@ module Scorecards
   class IndicatorsController < ApplicationController
     def index
       @scorecard = Scorecard.find(params[:scorecard_id])
-
-      predefineds = @scorecard.raised_indicators.where(indicatorable_type: "Indicator").group(:indicatorable_id).count
-      customs = @scorecard.raised_indicators.where(indicatorable_type: "CustomIndicator").group(:indicatorable_id).count
-
-      @indicators = Indicator.where(id: predefineds.keys).as_json
-      @indicators.each do |item|
-        item["count"] = predefineds[item["id"]]
-      end
-
-      @custom_indicators = CustomIndicator.where(id: customs.keys)
+      @indicator = RaisedIndicatorService.new(@scorecard.uuid).indicators
     end
   end
 end
