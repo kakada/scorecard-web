4b3c13efb283992b10e183af33a8645af39b0f40
Sokly <hengsokly23@gmail.com>
2020-10-22 16:51:40 +0700

    add spec

--- spec/models/voting_indicator_spec.rb
+++ spec/models/voting_indicator_spec.rb
@@ -12,5 +12,6 @@
 require 'rails_helper'
 
 RSpec.describe VotingIndicator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:scorecard) }
+  it { is_expected.to belong_to(:indicatorable) }
 end
