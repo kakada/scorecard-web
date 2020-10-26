533548790bc6dcf36aadbf96cad8e5ea9b33002f
Sokly <hengsokly23@gmail.com>
2020-10-26 14:59:03 +0700

    indicator: display audio

--- lib/sample/raised_indicator.rb
+++ lib/sample/raised_indicator.rb
@@ -14,12 +14,26 @@ module Sample
 
       scorecard.number_of_female.to_i.times do |i|
         custom_indicator = scorecard.custom_indicators.create(
-          name: "custom indicator #{i}",
+          name: "custom_indicator_#{i}",
           audio: ""
         )
+        assign_audio(custom_indicator)
 
         scorecard.raised_indicators.create(indicatorable: custom_indicator)
       end
     end
+
+    private
+      def self.assign_audio(indicator)
+        audio = indicators.select { |file| file.split("/").last.split(".").first == indicator[:name] }.first
+        return if audio.nil? || !File.exist?(audio)
+
+        indicator.audio = Pathname.new(audio).open
+        indicator.save
+      end
+
+      def self.indicators
+        @indicators ||= Dir.glob(Rails.root.join("lib", "sample", "assets", "indicators", "*"))
+      end
   end
 end
