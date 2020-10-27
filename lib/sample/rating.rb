881508b3452cbcb66d818fb4f4c47bbd962ea2cf
Sokly <hengsokly23@gmail.com>
2020-10-27 17:44:41 +0700

    move median to array class

--- lib/sample/rating.rb
+++ lib/sample/rating.rb
@@ -21,20 +21,9 @@ module Sample
     private
       def self.update_voting_indicator_median(scorecard)
         scorecard.voting_indicators.each do |indi|
-          indi_scores = ::Rating.where(voting_indicator_id: indi.id).pluck(:score)
-          indi.update(median: median(indi_scores).ceil)
+          ratings = ::Rating.where(voting_indicator_id: indi.id).pluck(:score)
+          indi.update(median: ratings.median.ceil)
         end
-      end
-
-      def self.median(array, already_sorted = false)
-        return nil if array.empty?
-        array = array.sort unless already_sorted
-        m_pos = array.size / 2
-        array.size % 2 == 1 ? array[m_pos] : mean(array[m_pos-1..m_pos])
-      end
-
-      def self.mean(array)
-        array.inject(0) { |sum, x| sum + x } / array.size.to_f
       end
   end
 end
