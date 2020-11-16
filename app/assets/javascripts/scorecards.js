a77e03707656bcc256c33cf4c7e02cf5aabae098
Sokly <hengsokly23@gmail.com>
2020-11-16 10:41:07 +0700

    Scorecard: enhance copy code style

--- app/assets/javascripts/scorecards.js
+++ app/assets/javascripts/scorecards.js
@@ -11,14 +11,37 @@ CW.ScorecardsShow = (() => {
   function onClickBtnCopy() {
     $(document).off('click', '.btn-copy');
     $(document).on('click', '.btn-copy', function(event) {
-      $(this).prev('input.input-for-copy').select();
-      document.execCommand("copy");
-
       event.preventDefault();
-      $('.toast').toast('show')
+
+      copyCode(this);
+      handleNotifyMessage(this);
     })
   }
 
+  function copyCode(dom) {
+    $(dom).prev('input.input-for-copy').select();
+    document.execCommand("copy");
+  }
+
+  function handleNotifyMessage(dom) {
+    $(dom).hide();
+    showBtnCheck(dom);
+
+    setTimeout(function() {
+      hideBtnCheck(dom);
+      $(dom).show();
+    }, 500);
+  }
+
+  function showBtnCheck(dom) {
+    $(dom).next('.btn-check').removeClass('d-none');
+    $(dom).next('.btn-check').tooltip('show');
+  }
+
+  function hideBtnCheck(dom) {
+    $(dom).next('.btn-check').tooltip('hide');
+    $(dom).next('.btn-check').addClass('d-none');
+  }
 
   function showScorecardModal() {
     !!$("#scorecardModal") && $("#scorecardModal").modal('show');
