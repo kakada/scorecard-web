14d0f73dfb6a0d20ed3d685105b2a513ccf7e8d4
Sokly <hengsokly23@gmail.com>
2020-12-21 16:33:34 +0700

    apply action button to all index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_063205) do
+ActiveRecord::Schema.define(version: 2020_12_17_074236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_12_11_063205) do
     t.string "audio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "content"
   end
 
   create_table "languages", force: :cascade do |t|
