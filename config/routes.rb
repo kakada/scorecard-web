3ace50f729851d67238093e00028988a69494f6b
Sokly <hengsokly23@gmail.com>
2020-12-24 15:32:34 +0700

    add tooltip to action button

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "dashboard#show"
+  root to: "scorecards#index"
 
   devise_for :users, path: "/", controllers: { confirmations: "confirmations", omniauth_callbacks: "users/omniauth_callbacks" }
 
