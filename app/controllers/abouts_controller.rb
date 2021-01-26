1729ef4def97b1ab22a9f0bd44ceb8508a7b2cc6
Sokly <hengsokly23@gmail.com>
2021-01-26 16:41:32 +0700

    update about us, add favicon

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class AboutsController < ApplicationController
+  skip_before_action :authenticate_user!
+  layout :set_layout
+
   def show
   end
+
+  private
+    def set_layout
+      signed_in? ? "layouts/application" : "layouts/footer_less"
+    end
 end
