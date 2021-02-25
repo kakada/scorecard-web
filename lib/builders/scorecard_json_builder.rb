6c633d201355bd7924959dff87d068fd92a04334
Sokly <hengsokly23@gmail.com>
2021-02-25 16:12:57 +0700

    Scorecard: export with participant info

--- lib/builders/scorecard_json_builder.rb
+++ lib/builders/scorecard_json_builder.rb
@@ -12,6 +12,18 @@ class ScorecardJsonBuilder
     _scorecard["location"] = scorecard.location.try(:as_json)
     _scorecard["proposed_criterias"] = ::Scorecards::ProposedCriteria.new(scorecard).criterias
     _scorecard["voting_criterias"] = ::Scorecards::VotingCriteria.new(scorecard).criterias
+    _scorecard["participants"] = build_participant(scorecard)
     _scorecard
   end
+
+  private
+    def build_participant(scorecard)
+      participants = [ { type: "female", count: scorecard.participants.select { |participant| participant.gender == "female" }.length } ]
+
+      %w(disability minority poor_card youth).each do |type|
+        participants << { type: type, count: scorecard.participants.select { |participant| !!participant.send(type) }.length }
+      end
+
+      participants
+    end
 end
