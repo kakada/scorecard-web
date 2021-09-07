d7facb1839cc18d6e9dc7996dc8f16d9f0332147
radin reth <radin.reth@gmail.com>
2021-09-07 17:46:41 +0700

    Fixed date filter

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -3,8 +3,8 @@ class ActivityLog < ApplicationRecord
     scope = all
     scope = scope.where(http_format: params[:http_format]) if params[:http_format].present?
     scope = scope.where(http_method: params[:http_method]) if params[:http_method].present?
-    scope = scope.where("DATE(created_at) > ?", params[:start_date]) if params[:start_date].present?
-    scope = scope.where("DATE(created_at) < ?", params[:end_date]) if params[:end_date].present?
+    scope = scope.where("DATE(created_at) >= ?", params[:start_date]) if params[:start_date].present?
+    scope = scope.where("DATE(created_at) <= ?", params[:end_date])   if params[:end_date].present?
     scope
   end
 end
