99707ba65ce7068cf3eade1b80f0b3697d92ad0e
radin reth <radin.reth@gmail.com>
2021-09-07 15:57:51 +0700

    Add basic filter

--- app/controllers/activity_logs_controller.rb
+++ app/controllers/activity_logs_controller.rb
@@ -1,5 +1,11 @@
 class ActivityLogsController < ApplicationController
   def index
-    @pagy, @activity_logs = pagy(ActivityLog.order(created_at: :desc))
+    @pagy, @activity_logs = pagy(ActivityLog.filter(activity_log_params).order(created_at: :desc))
+  end
+
+  private
+
+  def activity_log_params
+    params.permit(:http_format, :http_method, :start_date, :end_date)
   end
 end
--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -1,2 +1,10 @@
 class ActivityLog < ApplicationRecord
+  def self.filter(params = {})
+    scope = all
+    scope = scope.where(format: params[:http_format]) if params[:http_format].present?
+    scope = scope.where(http_method: params[:http_method]) if params[:http_method].present?
+    scope = scope.where("DATE(created_at) > ?", params[:start_date]) if params[:start_date].present?
+    scope = scope.where("DATE(created_at) < ?", params[:end_date]) if params[:end_date].present?
+    scope
+  end
 end
