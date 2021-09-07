767c34829264b6c8cf0d631934139c8d52cc3022
radin reth <radin.reth@gmail.com>
2021-09-07 14:26:22 +0700

    Controller activity log with ENV

---
+++ config/initializers/activity_logs_subscription.rb
@@ -0,0 +1,19 @@
+ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, started, finished, unique_id, data|
+  args = data.slice(:controller, :action, :format, :method, :path, :status)
+  args[:payload] = data[:params].except(:action, :controller)
+
+  if log_whitelist?(args[:controller])
+    ActivityLogsWorker.perform_async(args)
+  end
+end
+
+def log_whitelist?(controller_name)
+  log_controllers.any? do |item| 
+    controller_name.downcase.include?(item)
+  end
+end
+
+def log_controllers
+  controllers = ENV['ACTIVITY_LOGS_CONTROLLERS'].to_s
+  controllers.split(",")
+end
