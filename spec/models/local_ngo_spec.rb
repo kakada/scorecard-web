88a6cfce9249712cd97e8c54a243b6904955b5cc
radin reth <radin.reth@gmail.com>
2021-09-09 16:57:16 +0700

    Validate website url spec

--- spec/models/local_ngo_spec.rb
+++ spec/models/local_ngo_spec.rb
@@ -25,7 +25,6 @@ RSpec.describe LocalNgo, type: :model do
   it { is_expected.to have_many(:scorecards) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_uniqueness_of(:name).scoped_to(:program_id) }
-  it { is_expected.to validate_url_of(:website_url) }
 
   describe "before_save, set target_provinces" do
     let!(:ngo) { create(:local_ngo, target_province_ids: "01,02") }
@@ -33,5 +32,16 @@ RSpec.describe LocalNgo, type: :model do
     let!(:province2) { Pumi::Province.find_by_id("02") }
 
     it { expect(ngo.target_provinces).to eq("#{province1.name_km}, #{province2.name_km}") }
+  end
+
+  describe "#website_url" do
+    subject { build(:local_ngo, website_url: 'htp://invalidurl') }
+
+    it "validates website_url" do
+      subject.valid?
+
+      expect(subject).not_to be_valid
+      expect(subject.errors[:website_url]).to eq ["is invalid"]
+    end
   end
 end
