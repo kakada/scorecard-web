d97c4c9dc16f4ab0e1614b894beb365cb4a7158e
radin reth <radin.reth@gmail.com>
2021-09-20 17:07:27 +0700

    Refactor activity log

--- config/initializers/activity_logs_subscription.rb
+++ config/initializers/activity_logs_subscription.rb
@@ -1,6 +1,4 @@
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, started, finished, unique_id, data|
-  args = data.slice(:controller, :action, :format, :method, :path, :status, :current_user_id)
-  args[:payload] = data[:params].except(:action, :controller)
-
-  ActivityLogsEventHandler.new(args).process
+  request_log = RequestLogParser.parse(data)
+  ActivityLoggerService.log(request_log)
 end
---
+++ spec/parsers/request_log_parser_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe RequestLogParser do
+  describe "#parse" do
+    let(:parser) { described_class.parse(data) }
+
+    context "with whitelist attributes" do
+      let(:data) { { controller: "TestController" } }
+
+      specify { expect(parser).to have_key(:controller) }
+    end
+
+    context "without whitelist attributes" do
+      let(:data) { { bad_key: "BadKey" } }
+
+      specify { expect(parser).not_to have_key(:bad_key) }
+    end
+
+    context "with payload" do
+      let(:data) { { params: { controller: "TestController", action: "test", start_date: "01/02/2021" } } }
+
+      specify { expect(parser).to have_key(:start_date) }
+      specify { expect(parser).not_to have_key(:controller) }
+      specify { expect(parser).not_to have_key(:action) }
+    end
+  end
+end
