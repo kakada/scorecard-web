afb0c832c4c4f59cc59eaa08fa8d9b791470f1b9
radin reth <radin.reth@gmail.com>
2021-10-18 17:51:33 +0700

    Remove Ahoy

--- config/initializers/activity_logs_subscription.rb
+++ config/initializers/activity_logs_subscription.rb
@@ -2,6 +2,5 @@
 
 ActiveSupport::Notifications.subscribe "process_action.action_controller" do |name, started, finished, unique_id, data|
   request_log = RequestLogParser.parse(data)
-
   ActivityLoggerService.log(request_log)
 end
