f9ffeb817bbb9724f339d867ff624f4acb87f646
Sokly <hengsokly23@gmail.com>
2022-05-16 10:54:11 +0700

    Db backup: update gem to version 5

--- Backup/models/db_backup.rb
+++ Backup/models/db_backup.rb
@@ -1,51 +1,15 @@
 # encoding: utf-8
 
 ##
-# Backup Generated: my_backup
+# Backup Generated: db_backup
 # Once configured, you can run the backup with the following command:
 #
-# $ backup perform -t my_backup [-c <path_to_configuration_file>]
+# $ backup perform -t db_backup [-c <path_to_configuration_file>]
 #
-Backup::Model.new(:db_backup, 'Backup scorecards database') do
-  ##
-  # Split [Splitter]
-  #
-  # Split the backup file in to chunks of 250 megabytes
-  # if the backup file size exceeds 250 megabytes
-  #
-  split_into_chunks_of 250
-  ##
-  # Archive [Archive]
-  #
-  # Adding a file or directory (including sub-directories):
-  #   archive.add "/path/to/a/file.rb"
-  #   archive.add "/path/to/a/directory/"
-  #
-  # Excluding a file or directory (including sub-directories):
-  #   archive.exclude "/path/to/an/excluded_file.rb"
-  #   archive.exclude "/path/to/an/excluded_directory
-  #
-  # By default, relative paths will be relative to the directory
-  # where `backup perform` is executed, and they will be expanded
-  # to the root of the filesystem when added to the archive.
-  #
-  # If a `root` path is set, relative paths will be relative to the
-  # given `root` path and will not be expanded when added to the archive.
-  #
-  #   archive.root '/path/to/archive/root'
-  #
-  # For more details, please see:
-  # https://github.com/meskyanichi/backup/wiki/Archives
-  #
-  # archive :my_archive do |archive|
-  #   # Run the `tar` command using `sudo`
-  #   # archive.use_sudo
-  #   archive.add "/path/to/a/file.rb"
-  #   archive.add "/path/to/a/folder/"
-  #   archive.exclude "/path/to/a/excluded_file.rb"
-  #   archive.exclude "/path/to/a/excluded_folder"
-  # end
-
+# For more information about Backup's components, see the documentation at:
+# http://backup.github.io/backup
+#
+Model.new(:db_backup, 'Backup scorecards database') do
   database PostgreSQL do |db|
     # To dump all databases, set `db.name = :all` (or leave blank)
     db.name               = ENV.fetch('DB_NAME') { 'csc_web_development' }
@@ -67,6 +31,7 @@ Backup::Model.new(:db_backup, 'Backup scorecards database') do
     store_with Local do |local|
       local.path       = "~/backups/"
       local.keep       = 5
+      # local.keep       = Time.now - 2592000 # Remove all backups older than 1 month.
     end
   else
     ##
@@ -89,28 +54,5 @@ Backup::Model.new(:db_backup, 'Backup scorecards database') do
   # Gzip [Compressor]
   #
   compress_with Gzip
-
-  ##
-  # Mail [Notifier]
-  #
-  # The default delivery method for Mail Notifiers is 'SMTP'.
-  # See the Wiki for other delivery options.
-  # https://github.com/meskyanichi/backup/wiki/Notifiers
-  #
-  # notify_by Mail do |mail|
-  #   mail.on_success           = true
-  #   mail.on_warning           = true
-  #   mail.on_failure           = true
-
-  #   mail.from                 = "sender@email.com"
-  #   mail.to                   = "receiver@email.com"
-  #   mail.address              = "smtp.gmail.com"
-  #   mail.port                 = 587
-  #   mail.domain               = "your.host.name"
-  #   mail.user_name            = "sender@email.com"
-  #   mail.password             = "my_password"
-  #   mail.authentication       = "plain"
-  #   mail.encryption           = :starttls
-  # end
 
 end
