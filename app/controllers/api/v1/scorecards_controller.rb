ee81a4832ced18bab7dbe246fc4a1146a5230084
Sokly <hengsokly23@gmail.com>
2022-08-02 18:06:40 +0700

    Scorecard: add number of anonymous

--- app/controllers/api/v1/scorecards_controller.rb
+++ app/controllers/api/v1/scorecards_controller.rb
@@ -47,7 +47,7 @@ module Api
             :conducted_date, :number_of_caf, :number_of_participant, :number_of_female,
             :number_of_disability, :number_of_ethnic_minority, :number_of_youth, :number_of_id_poor,
             :finished_date, :language_conducted_code, :running_date, :device_type, :device_token,
-            :proposed_indicator_method,
+            :proposed_indicator_method, :number_of_anonymous,
             facilitators_attributes: [ :id, :caf_id, :position, :scorecard_uuid ],
             participants_attributes: [ :uuid, :age, :gender, :disability, :minority, :youth, :poor_card, :scorecard_uuid, :countable ],
             raised_indicators_attributes: [
