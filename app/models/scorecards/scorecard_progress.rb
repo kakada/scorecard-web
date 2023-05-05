bcb217ed4f82b45e4e84c4136305d3bc643e5bbd
Sokly <hengsokly23@gmail.com>
2023-05-05 13:34:45 +0700

    Scorecard list: display total number by each progress

---
+++ app/models/scorecards/scorecard_progress.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Scorecards
+  class ScorecardProgress
+    attr_reader :scorecards
+
+    def initialize(scorecards = [])
+      @scorecards = scorecards
+    end
+
+    def group_count
+      OpenStruct.new(
+        all: scorecards.length,
+        planned: scorecards.select { |s| s.planned? || s.renewed? }.length,
+        running: scorecards.select(&:running?).length,
+        in_review: scorecards.select(&:in_review?).length,
+        completed: scorecards.select(&:completed?).length
+      )
+    end
+  end
+end
