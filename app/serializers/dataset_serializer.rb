61718d03ca120d317c7d2ea1f1120734bdae827a
hengsokly <hengsokly23@gmail.com>
2025-12-16 09:12:02 +0700

    Close #513 add category name_en and name_km to dataset serializer

--- app/serializers/dataset_serializer.rb
+++ app/serializers/dataset_serializer.rb
@@ -2,9 +2,13 @@
 
 class DatasetSerializer < ActiveModel::Serializer
   attributes :id, :code, :name_en, :name_km, :province_id,
-             :district_id, :commune_id, :category_id, :category_name
+             :district_id, :commune_id, :category_id, :category_name_en, :category_name_km
 
-  def category_name
-    object.category&.name
+  def category_name_en
+    object.category&.name_en
+  end
+
+  def category_name_km
+    object.category&.name_km
   end
 end
